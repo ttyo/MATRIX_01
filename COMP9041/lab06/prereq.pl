fec64545e67f0fa165c9b8d9c32a82a5f585b6ff
Dylan Hsu <nonemaw@users.noreply.github.com>
2015-09-01 13:59:17 +1000

    Create prereq.pl

---
+++ COMP9041/lab06/prereq.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+
+$flag = 0;
+if ($ARGV[0] eq "-r") {
+        $flag = 1;
+        &prereq($ARGV[1]);
+}
+else {
+        &prereq($ARGV[0]);
+}
+
+
+$course = $ARGV[0];
+$url1 = "http://www.handbook.unsw.edu.au/postgraduate/courses/2015/$course.html";
+$url2 = "http://www.handbook.unsw.edu.au/undergraduate/courses/2015/$course.html";
+@pre_list = ();
+
+open F, "wget -q -O- $url1 $ url2|" or die;
+while ($line = <F>) {
+        if ($line =~ /.*Prerequisite:.*/) {
+                $line =~ s/\<\/p\>\<p\>\<strong\>Excluded\:.+\<\/a\>\<\/p\>$//g;
+                #$line =~ /[A-Z]{4}[0-9]{4}/gi;
+                #@pre_list = $line; QUESTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                @pre_list = $line =~ /[A-Z]{4}[0-9]{4}/gi;
+                for $pre (@pre_list) {
+                        print "$pre\n";
+                }
+        }
+}
